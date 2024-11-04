e28f232d97f62fd798310472cefe710b7d90e8d5
kght6123 <i.feeling.song+github@gmail.com>
2024-11-04 23:29:18 +0900

    update cli base.

--- dist/index.mjs
+++ dist/index.mjs
@@ -1,18 +1,1 @@
-import { createJiti } from "../node_modules/jiti/lib/jiti.mjs";
-
-const jiti = createJiti(import.meta.url, {
-  "interopDefault": true,
-  "alias": {
-    "techbook-template-cli": "/Volumes/Develop/techbook-template-cli"
-  },
-  "transformOptions": {
-    "babel": {
-      "plugins": []
-    }
-  }
-})
-
-/** @type {import("/Volumes/Develop/techbook-template-cli/src/index.js")} */
-const _module = await jiti.import("/Volumes/Develop/techbook-template-cli/src/index.ts");
-
-export default _module;
+console.log("Hello, world!");
