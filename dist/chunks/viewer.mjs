3f1ed923ad23d63067878b5c88acce67a7e324f7
kght6123 <i.feeling.song+github@gmail.com>
2024-11-11 23:53:26 +0900

    update viewer.

--- dist/chunks/viewer.mjs
+++ dist/chunks/viewer.mjs
@@ -1,10 +1,25 @@
 import express from 'express';
+import { m as distDir } from '../shared/techbook-template-cli.CQ5021rz.mjs';
+import '@akebifiky/remark-simple-plantuml';
+import '@shikijs/rehype';
+import '@shikijs/transformers';
+import 'rehype-mermaid';
+import 'rehype-slug';
+import 'rehype-stringify';
+import 'remark-frontmatter';
+import 'remark-gfm';
+import 'remark-parse';
+import 'remark-rehype';
+import 'unified';
+import 'vfile-matter';
+import 'qrcode';
+import 'jiti';
 
 async function main({ port = 3e3 }) {
   const app = express();
-  app.use("/dist/dist", express.static("./dist"));
-  app.use("/dist/images", express.static("./images"));
-  app.use("/dist", express.static("./dist"));
+  app.use(distDir + "/dist", express.static("./dist"));
+  app.use(distDir + "/dist/images", express.static("./images"));
+  app.use(distDir + "/dist", express.static("./dist"));
   app.use(express.static("./node_modules/@vivliostyle/viewer/lib"));
   console.log(
     `preview: http://localhost:${port}/index.html#src=/dist/publication.json&bookMode=true&renderAllPages=true&style=/dist/global.css&style=data:,/*%3Cviewer%3E*/%40page%7Bsize%3AJIS-B5%3B%7D/*%3C/viewer%3E*/&f=epubcfi(/6!)`
--- src/viewer.ts
+++ src/viewer.ts
@@ -1,12 +1,13 @@
 import express from "express";
+import { distDir } from "./constants";
 
 export default async function main({ port = 3000 }: { port: number }) {
   const app = express();
   
   // MEMO: VivlioStyle CLIに戻せるように現状ほパス変換を行っている。buildも含めた移行時に再検討する。
-  app.use("/dist/dist", express.static("./dist"));
-  app.use("/dist/images", express.static("./images"));
-  app.use("/dist", express.static("./dist"));
+  app.use(distDir + "/dist", express.static("./dist"));
+  app.use(distDir + "/dist/images", express.static("./images"));
+  app.use(distDir + "/dist", express.static("./dist"));
   
   app.use(express.static("./node_modules/@vivliostyle/viewer/lib"));
   
